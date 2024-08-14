d329f30ed63eafc17e1b0639f74b35d095dd2396
cybersecuritydef <155002701+cybersecuritydef@users.noreply.github.com>
2024-08-14 14:01:27 +0300

    Update wordlists.h

--- wordlists.h
+++ wordlists.h
@@ -3,25 +3,25 @@
 
 #include "common.h"
 
-struct wordlists{
+typedef struct{
     struct list *words;
     size_t count;
-};
+}wordlists;
 
-struct dictlists{
+typedef struct{
     struct list *dicts;
     size_t count;
-};
+}dictlists;
 
 
-void add_dictlists(struct dictlists *dict, const char *value);
+void add_dictlists(dictlists *dict, const char *value);
 
-void add_wordlists(struct wordlists *word, const char *value);
+void add_wordlists(wordlists *word, const char *value);
 
-struct wordlists *read_wordlists(const char *filename);
+wordlists *read_wordlists(const char *filename);
 
-void free_dictlists(struct dictlists *dict);
+void free_dictlists(dictlists *dict);
 
-void free_wordlists(struct wordlists *word);
+void free_wordlists(wordlists *word);
 
 #endif
