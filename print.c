a1dc75214d5051c8f82743eac3660a33873fb9f9
cybersecuritydef <155002701+cybersecuritydef@users.noreply.github.com>
2024-08-22 13:21:12 +0300

    Update print.c

--- print.c
+++ print.c
@@ -4,3 +4,12 @@
 #include <ctype.h>
 
 #include "print.h"
+#include "filter.h"
+
+void print(const char *url, const filters *filter, const response *resp, const bool body){
+    if((! number_filter(filter->hcode, resp->code)) && (! number_filter(filter->hsize, resp->len)) && (! string_filter(filter->hword, resp->content))){
+        printf("%s [code: %ld size: %ld time: %.1f]\n", url, resp->code, resp->len, resp->total_time);
+        if(body && resp->content != NULL)
+            printf("%s", resp->content);
+    }
+}
