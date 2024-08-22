222b3d82c1b317a603d8a0fefda203a721d34ec1
cybersecuritydef <155002701+cybersecuritydef@users.noreply.github.com>
2024-08-22 13:12:54 +0300

    Update fuzzer.c

--- fuzzer.c
+++ fuzzer.c
@@ -7,7 +7,7 @@
 
 static void print(const char *url, const filters *filter, const response *resp, const bool body){
     if((! number_filter(filter->hcode, resp->code)) && (! number_filter(filter->hsize, resp->len)) && (! string_filter(filter->hword, resp->content))){
-        printf("%s [code: %ld size: %ld time: %.1f]\n", url, resp->code,resp->len, resp->total_time);
+        printf("%s [code: %ld size: %ld time: %.1f]\n", url, resp->code, resp->len, resp->total_time);
         if(body && resp->content != NULL)
             printf("%s", resp->content);
     }
