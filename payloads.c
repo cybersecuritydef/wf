e8387c54c0e7a017f1c326e0afca47b96cd636cc
cybersecuritydef <155002701+cybersecuritydef@users.noreply.github.com>
2024-08-23 12:04:39 +0300

    Create payloads.c

---
+++ payloads.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sttring.h>
+
+#include "payloads.h"
+#include "common.h"
+#include "list.h"
+
+payloads *make_payloads(const char *url, wordlists *words, extlists *exts){
+  
+}
