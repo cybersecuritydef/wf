e6a40acbd388d0521df6b5228a006f99e167c130
cybersecuritydef <155002701+cybersecuritydef@users.noreply.github.com>
2024-08-23 12:03:06 +0300

    Update payloads.h

--- payloads.h
+++ payloads.h
@@ -1,8 +1,10 @@
 #ifndef __PAYLOADS_H__
 #define __PAYLOADS_H__
 typedef struct{
-  struct list *payload;
-  size_t count;
+    struct list *payload;
+    size_t count;
 }payloads;
+
+payloads *make_payloads(const char *url, wordlists *words, extlists *exts);
 
 #endif
