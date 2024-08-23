0ea77b790b059d8194e60f60bba38b5d4425e8ba
cybersecuritydef <155002701+cybersecuritydef@users.noreply.github.com>
2024-08-23 12:23:43 +0300

    Update payloads.c

--- payloads.c
+++ payloads.c
@@ -7,5 +7,20 @@
 #include "common.h"
 
 payloads *make_payloads(const char *url, const char *wordlist, const char *extlist){
+    wordlists *words = NULL;
+    extlists *exts = NULL;
+    payloads *payload = NULL;
   
+    if(url == NULL)
+      return NULL;
+  
+    if(words = read_wordlists(wordlist) == NULL)
+      return NULL;
+  
+    if(extlist != NULL){
+      if(exts = read_wordlists(extlist) == NULL)
+        return NULL;
+    }
+    
+    return payload;
 }
