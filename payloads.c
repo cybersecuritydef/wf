5afca012236df5410ea45d515f2417cf2b11d733
cybersecuritydef <155002701+cybersecuritydef@users.noreply.github.com>
2024-08-24 19:09:30 +0300

    Update payloads.c

--- payloads.c
+++ payloads.c
@@ -1,25 +1,10 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include "payloads.h"
 
-static char *join_payloads(const char *fp, const char *sp){
-    char *payload = NULL;
-    size_t flen = 0;
-    size_t slen = 0;
-    if(fp != NULL && sp != NULL){
-        flen = strlen(fp);
-        slen = strlen(sp);
-        if((payload = (char*)calloc((flen + slen + 1), sizeof(char))) != NULL){
-            memcpy(payload, fp, flen);
-            memcpy(payload + flen, sp, slen);
-            payload[flen + slen] = '\0';
-            return payload;
-        }
-    }
-    return NULL;
-}
 
 
 int make_payloads(const char *url, const char *wordlist, const char *extlist, payloads *payload){
@@ -29,7 +14,7 @@ int make_payloads(const char *url, const char *wordlist, const char *extlist, pa
     if((fword = fopen(wordlist, "r")) != NULL){
         while(fgets(buf, sizeof(buf), fword) != NULL){
             buf[strlen(buf) - 1] = '\0';
-            p = join_payloads(url, buf);
+            asprintf(&p, "%s%s", url, buf);
             payload->payload = add_first(payload->payload, p);
             payload->count += 1;
             free(p);
