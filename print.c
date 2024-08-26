3328ce70ce13463d8fe278ff453867db6986af46
cybersecuritydef <155002701+cybersecuritydef@users.noreply.github.com>
2024-08-26 17:14:25 +0300

    Update print.c

--- print.c
+++ print.c
@@ -9,9 +9,9 @@
 void print(const char *url, const filters *filter, const response *resp, const bool body){
     if((! number_filter(filter->hcode, resp->code)) && (! number_filter(filter->hsize, resp->len)) && (! string_filter(filter->hword, resp->content))){
         if(resp->code >= 400 && resp->code <= 526)
-            printf("[-] %-55s [code: %ld\tsize: %ld\ttime: %.1f]\n", url, resp->code, resp->len, resp->total_time);
+            printf("[-] %-55s [code: %ld \tsize: %ld \ttime: %.1f]\n", url, resp->code, resp->len, resp->total_time);
         else
-            printf("[+] %-55s [code: %ld\tsize: %ld\ttime: %.1f]\n", url, resp->code, resp->len, resp->total_time);
+            printf("[+] %-55s [code: %ld \tsize: %ld \ttime: %.1f]\n", url, resp->code, resp->len, resp->total_time);
         if(body && resp->content != NULL)
             printf("%s", resp->content);
     }
