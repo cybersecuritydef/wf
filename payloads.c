5526ea44fcf19a3a658016f78ad5bc58cde7171b
cybersecuritydef <155002701+cybersecuritydef@users.noreply.github.com>
2024-08-26 13:09:14 +0300

    Update payloads.c

--- payloads.c
+++ payloads.c
@@ -13,6 +13,8 @@ int make_payloads(const char *url, const char *wordlist, const char *extlist, pa
     char buf[LEN_BUF] = {'\0'};
     char *p = NULL;
     payloads *cur = NULL;
+    struct list *ext = NULL;
+    struct list *ext_cur = NULL;
     int status = 0;
     if((file = fopen(wordlist, "r")) != NULL){
         while(fgets(buf, sizeof(buf), file) != NULL){
@@ -28,19 +30,23 @@ int make_payloads(const char *url, const char *wordlist, const char *extlist, pa
         status = EOF;
     
     if(extlist != NULL){
-        if((file = fopen(extlist, "r")) != NULL){
-            cur = payload;
+        if((file = fopen(extlist, "r")) != NULL){            
             while(fgets(buf, sizeof(buf), file) != NULL){
                 buf[strlen(buf) - 1] = '\0';
-                while(cur != NULL){
-                    asprintf(&p, "%s%s", cur->payload->data, buf);
+                ext = add_first(ext, buf);                
+            }
+            fclose(file);
+            cur = payload;
+            while(cur != NULL){
+                ext_cur = ext;
+                while(ext_cur != NULL){
+                    asprintf(&p, "%s%s", cur->payload->data, ext_cur->data);
                     payload->payload = add_first(payload->payload, p);
                     payload->count += 1;
                     free(p);
-                    cur->payload = cur->payload->next;                    
+                    ext_cur = ext_cur->next;
                 }
-            }
-            fclose(file);
+                cur->payload = cur->payload->next;
         }
         else
             status = EOF;
